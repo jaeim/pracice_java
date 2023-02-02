024d1563cb1500e047e364c800c7098fb0f622dd
jaeim <thdwodla@gmail.com>
2023-02-02 23:24:15 +0900

    [add]선택정렬 -최소/최댓값 찾는 방법으로 변경

---
+++ src/JavaCodingTest/bfs/Bubble_Sort.java
@@ -0,0 +1,12 @@
+package JavaCodingTest.bfs;
+
+public class Bubble_Sort {
+    public static void main(String[] args) {
+        int[] arr = new int[]{3, 6, 41, 2, 4, 1, 5, 743};
+
+        for (int i = 0; i < arr.length; i++) {
+//            for (int j = 0; )
+        }
+
+    }
+}
