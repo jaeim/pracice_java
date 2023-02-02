cecd3c6a9f0d33b42bcd2c9b2dea92867f4da968
jaeim <thdwodla@gmail.com>
2023-02-03 00:24:03 +0900

    [add]버블정렬

--- src/JavaCodingTest/bfs/Bubble_Sort.java
+++ src/JavaCodingTest/bfs/Bubble_Sort.java
@@ -4,8 +4,17 @@ public class Bubble_Sort {
     public static void main(String[] args) {
         int[] arr = new int[]{3, 6, 41, 2, 4, 1, 5, 743};
 
-        for (int i = 0; i < arr.length; i++) {
-//            for (int j = 0; )
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++ ) {
+                if (arr[j] > arr[j + 1]) {
+                    int tmp = arr[j + 1];
+                    arr[j + 1] = arr[j];
+                    arr[j] = tmp;
+                }
+            }
+        }
+        for (int i : arr) {
+            System.out.print(i + " ");
         }
 
     }
